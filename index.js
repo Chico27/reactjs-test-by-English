1ca799681b1a001c9d3cda59c898189b83f739fd
Chico27 <masamasa.987654321@gmail.com>
2019-07-03 22:26:55 +0900

    new

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from "react"
 import ReactROM from "react-dom"
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"))
+// JSX
+ReactDOM.render(<div><h1>Hello world</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
